915acf6043bd7c1c2a145b45efc6c0f35da81418
Dray A <44196051+Purp1eW0lf@users.noreply.github.com>
2021-07-20 11:50:03 +0100

    Update SAM_Permissions_Check.ps1

--- SAM_Permissions/SAM_Permissions_Check.ps1
+++ SAM_Permissions/SAM_Permissions_Check.ps1
@@ -1,9 +1,11 @@
 ###Meta###
 	# Date: 2021 July 20th
 	# Authors: Tom Ellson & Dray Agha
+	# Find us on Twitter: https://twitter.com/tde_sec and https://twitter.com/Purp1eW0lf
 	# Org: JUMPSEC Labs
-	# Purpose: This script was written in response to the discovery that the SAM file had a permissions error that allows any user to read it's contents. This facilitates privilege escalation and as such, we quickly put this script together to help the community quickly determine how widespread their internal attack surface was for this.
 	# Contact: for more information, contact tellson@jumpsec.com or dray.agha@jumpsec.com
+
+	# Purpose: This script was written in response to the discovery that the SAM file had a permissions error that allows any user to read it's contents. This facilitates privilege escalation and as such, we quickly put this script together to help the community quickly determine how widespread their internal attack surface was for this.
 
 #Ensure errors don't ruin anything for us
 $ErrorActionPreference = "SilentlyContinue"
@@ -12,17 +14,16 @@ $ErrorActionPreference = "SilentlyContinue"
 Write-host "`nThis script will collect OS info and determine if the SAM file can be read by any user due to permissions error`n"
 write-host -foregroundcolor Magenta "`nRunning Script.......`n"
 
-
 ###collect computer information###
 	# Funnily enough, this takes the longest 
-
+	
 #Variables for OS version
 $Name = ([System.Net.Dns]::GetHostByName(($env:computerName))).Hostname
 $OS = gin | select -expandproperty OsName
 $Ver = gin | select -expandproperty WindowsVersion
 $Build= gin | select -expandproperty OSBuildNumber
 
-
+#demarcate OS Info section
 Write-host "`n---OS Info---"
 
 #print variables for OS version. Stupid formatting here to get the colours because I am extra
@@ -36,6 +37,7 @@ write-host -foregroundcolor Magenta "$Build`n"
 
 ###Determine if SAM is vulnerable###
 
+#demarcate results section
 write-host "`n---Vulnerability Results---"
 
 if ((get-acl C:\windows\system32\config\sam).Access |
@@ -45,3 +47,5 @@ if ((get-acl C:\windows\system32\config\sam).Access |
 	{write-host -foregroundcolor Red "`n$Name may be vulnerable: Arbitrary Read permissions for SAM file`n"}
 else {
 	write-host  -foregroundcolor Green "`n$Name does not seem to be vulnerable, SAM permissions are fine`n"}
+#The logic here is basic on filtering the permissions for the SAM file until we get the permissions string. The permissions string we alert as 'vuln' for is 'Read'.
+	# If the script cannot access the permissions, if the script gets a different permission string, it will return as 'not vuln', which should be accurate but of course explore this further. 
