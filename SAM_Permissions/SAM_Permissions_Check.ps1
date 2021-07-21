e9d1b347976b0e233e838eeb92d3daccfa81264d
Dray A <44196051+Purp1eW0lf@users.noreply.github.com>
2021-07-22 00:10:08 +0100

    Update SAM_Permissions_Check.ps1

--- SAM_Permissions/SAM_Permissions_Check.ps1
+++ SAM_Permissions/SAM_Permissions_Check.ps1
@@ -11,7 +11,7 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 #Print basic script info
-Write-host "`nThis script will collect OS info and determine if the SAM file can be read by any user due to permissions error`n"
+Write-host "`nThis script will collect OS info and determine if the SAM, SECURITY, SYSTEM files can be read by any user due to permissions error`n"
 write-host -foregroundcolor Magenta "`nRunning Script.......`n"
 
 ###collect computer information###
@@ -41,12 +41,17 @@ write-host -foregroundcolor Magenta "$Build`n"
 #demarcate results section
 write-host "`n---Vulnerability Results---"
 
-if ((get-acl C:\windows\system32\config\sam).Access |
+##for loop to collect permissions for SAM, SECURITY, and SYSTEM. Likely overkill, as the permissions for one are likely the permissions for all.
+$items = @("SAM", "SECURITY", "SYSTEM")
+foreach ($item in $items) {
+	$ErrorActionPreference = "SilentlyContinue" ;
+if ((get-acl C:\windows\system32\config\$item).Access |
 	? IdentityReference -match 'BUILTIN\\Users' | 
 	select -expandproperty filesystemrights | 
 	select-string 'Read')
-	{write-host -foregroundcolor Red "`n$Name may be vulnerable: Arbitrary Read permissions for SAM file`n"}
+	{write-host -foregroundcolor Red "`n$Name may be vulnerable: Arbitrary Read permissions for $item file`n"}
 else {
-	write-host  -foregroundcolor Green "`n$Name does not seem to be vulnerable, SAM permissions are fine`n"}
-#The logic here is basic on filtering the permissions for the SAM file until we get the permissions string. The permissions string we alert as 'vuln' for is 'Read'.
+	write-host  -foregroundcolor Green "`n$Name does not seem to be vulnerable, $item permissions are fine`n"}
+}
+#The logic here is basic on filtering the permissions for the SAM, security, and system files until we get the permissions string. The permissions string we alert as 'vuln' for is 'Read'.
 	# If the script cannot access the permissions, if the script gets a different permission string, it will return as 'not vuln', which should be accurate but of course explore this further. 
